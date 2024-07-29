c45afe6b5983b9790dd349fb69e3f50df7e58eb2
uwu <101582708+BarsikKotik@users.noreply.github.com>
2024-07-29 19:54:50 +0300

    Update speed converter background color

--- src/lib/calculators.ts
+++ src/lib/calculators.ts
@@ -47,8 +47,8 @@ export const calculators = writable<ICalculator[]>([
 		name: '',
 		localeKey: 'speed_converter',
 		url: 'speed-converter',
-		backgroundColor: '#a6f0c3',
-		backgroundColorDark: '#315e43',
+		backgroundColor: '#bdedcc',
+		backgroundColorDark: '#2e593f',
 		textColor: '#35a15f',
 		textColorDark: '#d1f4df'
 	}
